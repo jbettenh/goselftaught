399b37fbe9a76394ec63fa676f8fcb403bc4c9f9
jbettenh <21023218+jbettenh@users.noreply.github.com>
2020-08-29 10:05:10 +0200

    create and search linked list

---
+++ ch04_list_linked.py
@@ -0,0 +1,54 @@
+# Linked List
+import random
+
+class Node:
+    def __init__(self, data=None, next = None):
+        self.data = data
+        self.next = next
+
+    def print_node(self):
+        print(self.data)
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append_node(self, data):
+        if not self.head:
+            self.head = Node(data)
+            return
+        else:
+            current = self.head
+            while current.next:
+                current = current.next
+            current.next = Node(data)
+
+    def search(self, target):
+        current = self.head
+        while current is not None:
+            if current.data == target:
+                print("Found it!")
+                return True
+            else:
+                current = current.next
+                print("Not here.")
+        return False
+
+    def print_list(self):
+        node = self.head
+        while node is not None:
+            print(node.data, end = " ")
+            node = node.next
+        print("\n")
+
+
+
+
+# Create random number list
+the_list = LinkedList()
+
+for j in range(0, 20):
+    j = random.randint(1, 30)
+    the_list.append_node(j)
+the_list.print_list()
+the_list.search(10)
