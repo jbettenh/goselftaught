21ffa98faa268ee664b7779fe295be5b9f44acff
jbettenh <21023218+jbettenh@users.noreply.github.com>
2020-09-05 21:20:47 +0200

    oop shapes

--- ch02_oop.py
+++ ch02_oop.py
@@ -16,9 +16,6 @@ print(200.1)
 
 
 class Shape:
-    def __init__(self, w, l):
-        self.width = w
-        self.length = l
 
     def what_am_i(self):
         print("I am a shape!")
@@ -26,33 +23,61 @@ class Shape:
     def area(self):
         return self.width * self.length
 
-    def print_size(self):
-        print("""{} by {}""".format(self.width, self.length))
-
-    def calculate_perimeter(self):
-       print(2 * self.width + 2 * self.length)
-
 
 class Rectangle(Shape):
-   pass
+    recs = []
+
+    def __init__(self, w, l):
+        self.width = w
+        self.length = l
+        self.recs.append((self.width, self.length))
+
+    def calculate_perimeter(self):
+        print(2 * self.width + 2 * self.length)
+
+    def print_size(self):
+        print("""Rectangle: {} by {}""".format(self.width, self.length))
 
 
 class Square(Shape):
-    def change_size(self, amt):
-        if amt >= 0:
-            self.width = self.width + amt
-            self.length = self.length + amt
-        else:
-            self.width = self.width + amt
-            self.length = self.length + amt
+    def __init__(self, s):
+        self.s = s
 
+    def calculate_perimeter(self):
+        return self.s *4
+
+    def change_size(self, new_size):
+        self.s += new_size
+
+    def print_size(self):
+        print("""Square: {} by {}""".format(self.s, self.s))
+
+
+class Triangle(Shape):
+    def calculate_perimeter(self):
+        print(self.side_a + self.side_b + self.side_c)
+
+    def print_size(self):
+        print("""Triangle: {} by {} by {}""".format(self.side_a, self.side_b, self.side_c))
 
 
 a_rectangle = Rectangle(10, 5)
 a_rectangle.print_size()
 a_rectangle.calculate_perimeter()
 
-a_square = Square(2, 2)
+b_rectangle = Rectangle(17, 8)
+c_rectangle = Rectangle(9,11)
+a_rectangle.what_am_i()
+print(Rectangle.recs)
+
+a_square = Square(8)
+a_square.what_am_i()
 a_square.print_size()
+a_square.calculate_perimeter()
 a_square.change_size(-1)
+a_square.calculate_perimeter()
 a_square.print_size()
+
+a_triangle = Triangle(5, 7, 13)
+a_triangle.calculate_perimeter()
+a_triangle.print_size()
