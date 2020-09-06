4c072729985267b77d1dfdf4d231543ed886184c
jbettenh <21023218+jbettenh@users.noreply.github.com>
2020-09-06 21:03:44 +0200

    war game

--- ch02_war.py
+++ ch02_war.py
@@ -83,6 +83,30 @@ class Game:
             response = input(m)
             if response == 'q':
                 break
-                
 
+            p1c = self.deck.rm_card()
+            p2c = self.deck.rm_card()
+            p1n = self.p1.name
+            p2n = self.p2.name
+            self.draw(p1n, p1c, p2n, p2c)
+
+            if p1c > p2c:
+                self.p1.wins += 1
+                self.wins(self.p1.name)
+            else:
+                self.p2.wins += 1
+                self.wins(self.p2.name)
+
+        win = self.winner(self.p1, self.p2)
+        print("War is over. {} wins!".format(win))
+
+    def winner(self, p1, p2):
+        if p1.wins > p2.wins:
+            return p1.name
+        if p1.wins < p2.wins:
+            return p2.name
+        return "It was a tie!"
+
+game = Game()
+game.play_game()
 
