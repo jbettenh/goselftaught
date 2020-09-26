6b25170ea61548e7abca3d301dd57e8729a1ebd6
jbettenh <21023218+jbettenh@users.noreply.github.com>
2020-09-26 10:52:45 +0200

    hash table python dictionary

--- ch04_hash_table.py
+++ ch04_hash_table.py
@@ -3,5 +3,14 @@ A hash takes a number or characters and performs a arithmetic or logical transfo
 A simple hash is modulo division.
  Hash tables should be the length of prime numbers. Python Dictionaries use Hash Tables."""
 
-hlist = [86, 6, 22, 38, 68]
-
+# makeup list of IP addresses
+sockets = {'T1R': '10.0.0.142:5002', 'TR2': '192.168.3.55:5002'}
+print(sockets)
+sockets['TR3'] = '10.250.6.23:5002'
+sockets['TR4'] = '10.250.7.24:5002'
+sockets['CL3'] = '10.250.6.30:5002'
+print(sockets)
+sockets['TR4'] = '10.250.6.24:5002'
+print(sockets)
+del sockets['CL3']
+print(sockets)
